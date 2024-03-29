10b077113e5ff7c55592a11e5973e8c30b333bea
Mayank Kaushik <mkaushik0033@gmail.com>
2024-03-30 00:32:43 +0530

    installed madge

--- src/migrations/1711726452661-migration.ts
+++ src/migrations/1711726452661-migration.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1711726452661 implements MigrationInterface {
-    name = 'Migration1711726452661'
+  name = 'Migration1711726452661';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "example" ADD "school" character varying`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "example" ADD "school" character varying`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "example" DROP COLUMN "school"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "example" DROP COLUMN "school"`);
+  }
 }

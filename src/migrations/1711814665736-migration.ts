1296e2734d04834d1ca9e0b1c7c03102e9bff5c0
Mayank Kaushik <mkaushik0033@gmail.com>
2024-03-30 21:35:25 +0530

    Auto Lint Fixes

--- src/migrations/1711814665736-migration.ts
+++ src/migrations/1711814665736-migration.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1711814665736 implements MigrationInterface {
-    name = 'Migration1711814665736'
+  name = 'Migration1711814665736';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "testprop"`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "testprop"`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ADD "testprop" character varying DEFAULT 'testing propr'`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ADD "testprop" character varying DEFAULT 'testing propr'`
+    );
+  }
 }

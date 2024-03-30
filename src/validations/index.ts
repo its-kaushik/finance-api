93a78d94a0402008c6b66c38446edd287dbf35ad
Mayank Kaushik <mkaushik0033@gmail.com>
2024-03-30 18:59:09 +0530

    Add: Added some base validations

--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,5 +1,13 @@
 import Joi from 'joi';
 
+export const createValidation = (schema: any) => Joi.object(schema);
+
 export const stringRequired = Joi.string().required();
 
-export const createValidation = (schema: any) => Joi.object(schema);
+export const emailRequired = Joi.string().email().required();
+
+export function enumValidationRequired(enumValidation: string[] | number[]) {
+  return Joi.string()
+    .valid(...enumValidation)
+    .required();
+}
